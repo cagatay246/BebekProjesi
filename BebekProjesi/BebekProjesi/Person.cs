d8b1d69258c549a1d969378417b86feac8c23f31
cagatay246 <cagataykilgi7@gmail.com>
2025-01-13 11:20:42 +0300

    1

---
+++ BebekProjesi/BebekProjesi/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BebekProjesi
+{
+    class Person
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+
+        public Person()
+        {
+            Console.WriteLine("Ingaaaaaa");
+            DogumTarihi = DateTime.Now;
+        }
+
+        public Person (string ad,string soyad): this()
+        {
+            Ad = ad;
+            Soyad = soyad;
+        }
+            
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($"AD: {Ad} , Soyad: {Soyad}, DOĞUM TARİHİ: {DogumTarihi.ToShortDateString()}");
+        }
+
+    }
+
+  
+
+
+
+   
+}
---
+++ BebekProjesi/BebekProjesi/Program.cs
@@ -0,0 +1,22 @@
+﻿using BebekProjesi;
+
+class Program
+{
+    static void Main()
+    {
+
+        Person person1 = new Person("mehmet", "ateş ");
+        Person person2 = new Person();
+
+        Console.WriteLine("\nbebek 1 bilgileri");
+        person1.BilgileriYazdir();
+       
+        Console.WriteLine("\nbebek 2 bilgileri");
+        person2.BilgileriYazdir();
+
+
+
+
+
+    }
+}
